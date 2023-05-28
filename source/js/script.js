35e38944200e1bd868156c027c7d0ce43ed0048b
sFlcn <sfalcn@gmail.com>
2023-05-29 00:26:13 +0300

    visual features fixes

--- source/js/script.js
+++ source/js/script.js
@@ -78,7 +78,48 @@ function addTelInputMasks() {
   [].forEach.call(document.querySelectorAll('[type="tel"]'), telMask);
 }
 
+// animated appearance
+
+function isPartiallyVisible(element) {
+  const { bottom, height } = element.getBoundingClientRect();
+  return (bottom - height < window.innerHeight);
+}
+
+function animateAppearance(elementsCssClass, throttleTimer) {
+  const elementsList = document.querySelectorAll(`.${elementsCssClass}`);
+  if (!elementsList) { return; }
+
+  function scrollingResolve() {
+    elementsList.forEach((element) => {
+      if (isPartiallyVisible(element)) {
+        element.classList.remove(`${elementsCssClass}--hidden`);
+        element.classList.add(`${elementsCssClass}--shown`);
+      }
+    });
+  }
+
+  let isThrottle = false;
+
+  function throttledScroll() {
+    if (isThrottle) { return; }
+
+    isThrottle = true;
+
+    setTimeout(() => {
+      scrollingResolve(elementsList, elementsCssClass);
+      isThrottle = false;
+    }, throttleTimer);
+  }
+
+  elementsList.forEach((element) => { element.classList.add(`${elementsCssClass}--hidden`); });
+  window.addEventListener('scroll', throttledScroll);
+  document.addEventListener('DOMContentLoaded', scrollingResolve);
+}
+
+// LAUNCHING:
+
 window.addEventListener('DOMContentLoaded', addTelInputMasks);
+animateAppearance('animated-appearance', 250);
 
 basequipmentAccordions.forEach((el) => {
   animateAccordion(el, 'basequipment__item--open');
