740f488241b101013aec5f2ed64957666ac46841
sFlcn <sfalcn@gmail.com>
2023-10-15 14:56:28 +0300

    fix widget script

--- source/js/widget-headset.js
+++ source/js/widget-headset.js
@@ -1,11 +1,12 @@
 export default function headsetStick({ targetEl, cssClass, scrollDistance }) {
   const el = targetEl;
-  window.onscroll = function handeleStick() {
-    console.log(window.scrollY);
-    if (window.scrollY >= scrollDistance) {
-      el.classList.add(cssClass);
-    } else {
-      el.classList.remove(cssClass);
-    }
-  };
+  if (el) {
+    window.onscroll = function handeleStick() {
+      if (window.scrollY >= scrollDistance) {
+        el.classList.add(cssClass);
+      } else {
+        el.classList.remove(cssClass);
+      }
+    };
+  }
 }
